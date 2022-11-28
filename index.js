0c5fc12d7d152637a01d734a0368da97a0931404
williamalverio <111805116+williamalverio@users.noreply.github.com>
2022-11-28 16:28:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 document.querySelector("h1").addEventListener("click", function (){
-  document.body.style.color = "lightpink";
+  var colorRandom = Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.color = "#"+colorRandom;
 });
